edc4c5a8d20aa50d4dbac74fa3881f84fef1fea4
rewamp <rewamp.15@gmail.com>
2016-07-28 03:00:53 -0700

    Add files via upload

---
+++ form-to-email.php
@@ -0,0 +1,60 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = $visitor_email;//<== update the email address
+$email_subject = $subject;
+$email_body = "You have received a new message from the user $name.\n".
+ "Phone no: $phone".
+    "Here is the message:\n $message".
+    
+$to = "rewamp.15@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: http://www.rewamp.co.in/thank-you.php');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
---
+++ thank-you.php
@@ -0,0 +1,19 @@
+<?php
+header('Location: http://www.rewamp.co.in');
+?> 
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Thank you!</title>
+<meta name="generator" >
+</head>
+
+<body>
+<style>body {background: yellow;}</style>
+<H1>Thank You !!</H1>
+<style>H1 { color: #111; font-family: 'Helvetica Neue', sans-serif; font-size: 275px; font-weight: bold; letter-spacing: -1px; line-height: 1; text-align: center; }</syle>
+
+
+</body>
+</html>
